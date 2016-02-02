8f8be0de9cdea5c55cfcd1d206306191bc8a2f83
Maciej Szwaja <mszwaja@7bulls.com>
2016-02-02 17:19:00 +0100

    Correctly rename f command line arg

--- app/Main.hs
+++ app/Main.hs
@@ -17,20 +17,20 @@ realToComplex a = a :+ 0
 main :: IO ()
 main = do
   args <- getArgs
-  let _N = (read . head) args
+  let f = (read . head) args
       graphStroke = Stroke (Just "black") (Just 0.03)
-  for_ (zip (animateFreq (map realToComplex signal) _N) [0..])
+  for_ (zip (animateFreq (map realToComplex signal) f) [0..])
     (\(content, num) ->
       writeFile ("graph" ++ show num ++ ".svg") (
         prologue Nothing "xlink" (Just (WidthHeight "600" "600")) (Just $ ViewBox "-5" "-5" "10" "10") ++
         content ++
         "<g transform=\"translate(-4 -3) scale(" ++ show (8.0 / 256.0) ++ " 1)\">" ++
         drawGraph (zip [0..255] signal) (Just num) Nothing graphStroke ++
-        foldr1 (++) (map (\i -> let x = show (fromIntegral (i * 256) / (fromIntegral _N)) in
+        foldr1 (++) (map (\i -> let x = show (fromIntegral (i * 256) / (fromIntegral f)) in
                            "<" ++ maybePrefix Nothing ++
                            "line " ++ strokeToString graphStroke ++
                            " x1=\"0\" y1=\"1\" x2=\"0\" y2=\"-1\"" ++
-                           " transform=\"translate(" ++ x ++ " 0) scale(" ++ show (256.0 / 8.0) ++ " 1)\" />") [0.._N]) ++
+                           " transform=\"translate(" ++ x ++ " 0) scale(" ++ show (256.0 / 8.0) ++ " 1)\" />") [0..f]) ++
         "</g>" ++
         epilogue Nothing))
   
